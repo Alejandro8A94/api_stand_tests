4290ff0128478859be27ee103360456c88fe910d
Alejandro <ah3709551@gmail.com>
2024-04-12 15:49:27 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://cnt-9fc8b0b8-5805-4082-89ae-506912f107a4.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+# Solicitudes POST #
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
